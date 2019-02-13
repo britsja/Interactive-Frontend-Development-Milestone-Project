79f37db30011cf70b282285accf4644e03a3917f
Jan Brits <pcerservices@gmail.com>
2019-02-13 09:47:56 +0200

    Added tickets per technician

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 "# Interactive-Frontend-Development-Milestone-Project" 
+
+#might be a good idea to use this:
+Reset DC data button:https://steemit.com/utopian-io/@faad/tutorial-18-dive-into-dc-js-a-javascript-library-data-count-and-reset-last-tutorial
--- static/js/graph.js
+++ static/js/graph.js
@@ -8,6 +8,7 @@ function makeGraphs(error, tickets) {
     
     var ndx = crossfilter(tickets.results);
     show_ticket_type_selector(ndx);
+    show_tickets_per_technician(ndx);
     
     dc.renderAll();
 
@@ -24,4 +25,25 @@ function show_ticket_type_selector(ndx) {
         .dimension(dim)
         .group(group)
         .numberVisible(17);
+}
+
+// TICKETS PER TECHNICIAN
+
+function show_tickets_per_technician(ndx) {
+    var dim = ndx.dimension(dc.pluck('assignee_id'));
+    var group = dim.group();
+
+    dc.barChart("#tickets-technician")
+        .width(600)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Technician")
+        .yAxisLabel("Tickets")
+        .elasticY(true)
+        .yAxis().ticks(20);
 }
